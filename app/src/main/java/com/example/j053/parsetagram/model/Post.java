a3a2030ed5e6951501fe9a4edbbb46b59d1f6f31
jamendez99 <j053@fb.com>
2018-07-09 18:01:09 -0700

    Started creating Post model

---
+++ app/src/main/java/com/example/j053/parsetagram/model/Post.java
@@ -0,0 +1,37 @@
+package com.example.j053.parsetagram.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject{
+    private final static String KEY_DESCRIPTION = "description";
+    private final static String KEY_IMAGE = "image";
+    private final static String KEY_USER = "user";
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String s) {
+        put(KEY_DESCRIPTION, s);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile pf) {
+        put(KEY_IMAGE, pf);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+}
